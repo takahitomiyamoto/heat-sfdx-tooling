1288a43e28ed36fa494e2a8c07b5ba155a6e9151
Takahito Miyamoto <takahito0508+github@gmail.com>
2021-11-20 11:40:22 +0900

    :+1:

---
+++ src/custom-calls/spec-apex/build.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/**
+ * index.ts
+ */
+import { authorization } from 'heat-sfdx-common';
+import { asyncCreateMetadataContainerId } from './async-create';
+import { asyncRetrieveApex } from './async-retrieve';
+import { runBatch } from './batch';
+
+// The request in POST /tooling/composite can’t contain more than 25 operations.
+const COMPOSITE_OPERATIONS_LIMIT = 25;
+const MANAGED = ['beta', 'deleted', 'deprecated', 'released', 'installed'];
+
+/**
+ * @description buildApexSpecs
+ * @param auth
+ */
+export async function buildApexSpecs(auth: authorization) {
+  // create MetadataContainerId
+  auth.options.logs.createMetadataContainer =
+    auth.options.environment.logs.createMetadataContainer;
+  auth.options.metadataContainerId = await asyncCreateMetadataContainerId(auth);
+
+  // retrieve ApexClasses / ApexTriggers
+  const apexRecords = await asyncRetrieveApex(auth);
+
+  // run the batch operation because the request can’t contain more than 25 operations.
+  const apexRecordsNotManaged = apexRecords?.filter((r: any) => {
+    return !MANAGED.includes(r.ManageableState);
+  });
+  const size = apexRecordsNotManaged?.length;
+  const scope = COMPOSITE_OPERATIONS_LIMIT;
+  let start = 0;
+  while (start < Math.ceil(size / scope)) {
+    auth.options.apex = apexRecordsNotManaged.slice(start, start + scope - 1);
+
+    /**
+     * 1. create ApexClassMembers / ApexTriggerMembers
+     * 2. create ContainerAsyncRequest
+     * 3. retrieve ContainerAsyncRequest - State
+     * 4. retrieve ApexClassMembers / ApexTriggerMembers
+     * 5. archive .json
+     */
+    await runBatch(auth);
+    start += scope;
+  }
+
+  return;
+}
